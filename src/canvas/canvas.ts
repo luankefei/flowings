1c62904f13ebada22a330e3474c6ec9c43b61246
luankefei <luankefei@gmail.com>
2020-06-05 12:05:02 +0800

    feat: 增加图层合并模块canvas，及types定义

---
+++ src/canvas/canvas.ts
@@ -0,0 +1,6 @@
+/**
+ * canvas
+ *
+ * 图层合并业务的入口文件
+ * 因为原来的项目叫做 laiye-canvas 和 golang-canvas 所以这里承接了历史包袱
+ */
---
+++ src/interface/canvas.type.ts
@@ -0,0 +1,77 @@
+/**
+ * canvas.type
+ *
+ * 绘图合并相关的定义文件
+ */
+
+// Basic:  createImage 接口的第一个参数
+interface Basic {
+  width;
+  height;
+  image_type: number;
+  file_name;
+  business_name;
+  wechat_openid;
+  trace_id: string;
+  send: boolean;
+}
+
+// Image: 图片元素的配置
+interface Image {
+  x;
+  y;
+  z;
+  width;
+  height;
+  expire;
+  border_radius: number;
+  name;
+  image_url: string;
+  resize: boolean;
+  clip: Clip | undefined;
+}
+
+// Clip: 图片元素的裁剪配置
+interface Clip {
+  x;
+  y;
+  width;
+  height: number;
+}
+
+// Text: 文字元素的绘图配置
+interface Text {
+  x;
+  y;
+  z;
+  size;
+  line_height;
+  font_style;
+  align;
+  limit: number;
+  color;
+  font_family;
+  content: string;
+}
+
+// Line: 线条元素的绘制配置
+interface Line {
+  x1;
+  y1;
+  x2;
+  y2;
+  line_width: number;
+  color: string;
+  dashed: boolean;
+}
+
+// Rect: 矩形元素的绘制配置
+interface Rect {
+  x;
+  y;
+  width;
+  height: number;
+  name;
+  background: string;
+  radius: number[];
+}
