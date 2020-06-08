1c834d5cb16a193067ca84e0aa893a01e8e1fd4b
luankefei <luankefei@gmail.com>
2020-06-08 18:30:36 +0800

    feat(canvas): 增加图片和文字类

---
+++ src/canvas/image.ts
@@ -0,0 +1,14 @@
+/**
+ * 图片绘制
+ */
+class CImage {
+  constructor() {
+    console.log("image constructor");
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    console.log("draw image");
+  }
+}
+
+export default CImage;
---
+++ src/canvas/text.ts
@@ -0,0 +1,13 @@
+/**
+ * 文字绘制
+ */
+class CText {
+  constructor() {
+    console.log("text constructor");
+  }
+  draw(ctx: CanvasRenderingContext2D) {
+    console.log("draw text");
+  }
+}
+
+export default CText;
