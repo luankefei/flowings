e613ad7e65b0ba72db57611a6158c9563ef4a52e
luankefei <luankefei@gmail.com>
2020-06-08 11:15:40 +0800

    预留createCanvas的逻辑

--- src/canvas/canvas.ts
+++ src/canvas/canvas.ts
@@ -5,10 +5,21 @@
  * 因为原来的项目叫做 laiye-canvas 和 golang-canvas 所以这里承接了历史包袱
  */
 
+import LayerHelper from "../helper/layer.helper";
+
 // createImage: 对外暴露的合并图层API
 function createImage(basic: Basic, params: LayerWrapper) {
   console.log("basic", basic);
   console.log("params", params);
+
+  // 这里打算做成 layerHelper 的语法糖
+  const helper = new LayerHelper();
+  helper.layers = params;
+
+  // 注入 context
+  // helper.render();
+
+  // 使用 canvas 导出图片
 }
 
 export { createImage };
