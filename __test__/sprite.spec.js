d46711460778566fae62d36f92d5b8e440a4c5eb
luankefei <luankefei@gmail.com>
2020-06-10 18:26:01 +0800

    animation backup

--- __test__/sprite.spec.js
+++ __test__/sprite.spec.js
@@ -1,12 +1,12 @@
 import Flowings from '../src/main';
-import sprite from '../src/sprite';
+import Sprite from '../src/sprite';
 import { BallPainter } from '../src/painter';
 
 test('create a sprite', () => {
   const { canvas, context } = new Flowings();
 
   expect(() => {
-    const ball = new sprite('ball', new BallPainter(75));
+    const ball = new Sprite('ball', new BallPainter(75));
 
     ball.left = 320;
     ball.top = 160;
--- src/painter.ts
+++ src/painter.ts
@@ -1,93 +1,93 @@
-import Sprite from "./sprite";
+// import Sprite from './sprite';
 
-class ImagePainter {
-  image: HTMLImageElement;
+// class ImagePainter {
+//   image: HTMLImageElement;
 
-  constructor(image: HTMLImageElement) {
-    this.image = image;
-  }
+//   constructor(image: HTMLImageElement) {
+//     this.image = image;
+//   }
 
-  paint(sprite: Sprite, context: CanvasRenderingContext2D) {
-    context.drawImage(
-      this.image,
-      sprite.left,
-      sprite.top,
-      sprite.width,
-      sprite.height
-    );
-  }
-}
+//   paint(sprite: Sprite, context: CanvasRenderingContext2D) {
+//     context.drawImage(
+//       this.image,
+//       sprite.left,
+//       sprite.top,
+//       sprite.width,
+//       sprite.height
+//     );
+//   }
+// }
 
-interface ICell {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-}
+// interface ICell {
+//   x: number;
+//   y: number;
+//   width: number;
+//   height: number;
+// }
 
-class SpriteSheetPainter {
-  cells: ICell[];
-  cellIndex = 0;
-  spriteSheet: HTMLImageElement;
+// class SpriteSheetPainter {
+//   cells: ICell[];
+//   cellIndex = 0;
+//   spriteSheet: HTMLImageElement;
 
-  constructor(cells: any, spriteSheet: HTMLImageElement) {
-    this.cells = cells;
-    this.spriteSheet = spriteSheet;
-  }
+//   constructor(cells: any, spriteSheet: HTMLImageElement) {
+//     this.cells = cells;
+//     this.spriteSheet = spriteSheet;
+//   }
 
-  advance() {
-    // loop
-    if (this.cellIndex === this.cells.length - 1) {
-      this.cellIndex = 0;
-    } else {
-      this.cellIndex += 1;
-    }
-  }
+//   advance() {
+//     // loop
+//     if (this.cellIndex === this.cells.length - 1) {
+//       this.cellIndex = 0;
+//     } else {
+//       this.cellIndex += 1;
+//     }
+//   }
 
-  paint(sprite: sprite, context: canvasrenderingcontext2d) {
-    const cell = this.cells[this.cellIndex];
-    context.drawImage(
-      this.spriteSheet,
-      cell.x,
-      cell.y,
-      cell.width,
-      cell.height,
-      sprite.left,
-      sprite.top,
-      cell.width,
-      cell.height
-    );
-  }
-}
+//   paint(sprite: sprite, context: canvasrenderingcontext2d) {
+//     const cell = this.cells[this.cellIndex];
+//     context.drawImage(
+//       this.spriteSheet,
+//       cell.x,
+//       cell.y,
+//       cell.width,
+//       cell.height,
+//       sprite.left,
+//       sprite.top,
+//       cell.width,
+//       cell.height
+//     );
+//   }
+// }
 
-class BallPainter {
-  radius: number;
+// class BallPainter {
+//   radius: number;
 
-  constructor(radius: number) {
-    this.radius = radius;
-  }
+//   constructor(radius: number) {
+//     this.radius = radius;
+//   }
 
-  paint(sprite: Sprite, context: CanvasRenderingContext2D) {
-    context.beginPath();
-    context.arc(
-      sprite.left + sprite.width / 2,
-      sprite.top + sprite.height / 2,
-      this.radius,
-      0,
-      Math.PI * 2,
-      false
-    );
-    context.clip();
-    context.shadowColor = "rgb(0,0,0)";
-    context.shadowOffsetX = -4;
-    context.shadowOffsetY = -4;
-    context.shadowBlur = 8;
-    context.lineWidth = 2;
-    context.strokeStyle = "rgb(100,100,195)";
-    context.fillStyle = "rgba(30, 144,255,0.15)";
-    context.fill();
-    context.stroke();
-  }
-}
+//   paint(sprite: Sprite, context: CanvasRenderingContext2D) {
+//     context.beginPath();
+//     context.arc(
+//       sprite.left + sprite.width / 2,
+//       sprite.top + sprite.height / 2,
+//       this.radius,
+//       0,
+//       Math.PI * 2,
+//       false
+//     );
+//     context.clip();
+//     context.shadowColor = 'rgb(0,0,0)';
+//     context.shadowOffsetX = -4;
+//     context.shadowOffsetY = -4;
+//     context.shadowBlur = 8;
+//     context.lineWidth = 2;
+//     context.strokeStyle = 'rgb(100,100,195)';
+//     context.fillStyle = 'rgba(30, 144,255,0.15)';
+//     context.fill();
+//     context.stroke();
+//   }
+// }
 
-export { ImagePainter, SpriteSheetPainter, BallPainter };
+// export { ImagePainter, SpriteSheetPainter, BallPainter };
