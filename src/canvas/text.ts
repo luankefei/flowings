37344d0d84f19f091c0557e686be99c3aae7bb41
luankefei <luankefei@gmail.com>
2021-11-01 18:04:02 +0800

    feat(text): 支持文字折行，对齐，文字颜色

--- src/canvas/text.ts
+++ src/canvas/text.ts
@@ -1,7 +1,6 @@
 /**
  * 文字绘制
  */
-
 import { IText } from "../interface/canvas.type";
 
 class CText implements IText {
@@ -14,7 +13,7 @@ class CText implements IText {
   font_weight = "normal";
   align = "left" as CanvasTextAlign;
   limit = 0;
-  color = "";
+  color = "#000";
   font_family = "sans-serif";
   content = "";
   baseline = "top" as CanvasTextBaseline;
@@ -28,19 +27,24 @@ class CText implements IText {
     ctx.font = `normal normal ${this.font_weight} ${this.size}px ${this.font_family}`;
     ctx.textAlign = this.align;
     ctx.textBaseline = this.baseline;
-    ctx.fillText(this.content, this.x, this.y);
+    ctx.fillStyle = this.color;
 
-    // 文字颜色
-    if (this.color) {
-      ctx.fillStyle = this.color;
+    // 按照对齐方式，修正坐标起点
+    if (this.align !== "left") this.x = this.align === "center" ? this.x + this.limit / 2 : this.x + this.limit;
+    if (!this.limit) return ctx.fillText(this.content, this.x, this.y);
+
+    // 多行文本渲染
+    const charWidth = ctx.measureText(this.content).width / this.content.length;
+
+    // 按照当前文字宽度计算余数矫正一次
+    const limit = Math.floor(this.limit / charWidth);
+    const lineCount = Math.ceil(this.content.length / limit);
+
+    for (let i = 0; i < lineCount; i += 1) {
+      const line = this.content.substring(limit * i, limit * i + limit);
+      ctx.fillText(line, this.x, this.y + i * this.line_height);
     }
 
-    console.log(
-      "------------ draw text",
-      this.content,
-      ctx.font,
-      `normal normal ${this.font_weight} ${this.size}px ${this.font_family}`
-    );
     ctx.restore();
   }
 }
